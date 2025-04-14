bd727c021162e6ef73380f558ab4aada3392278e
Kevin Tchinda <kevinntchinda@outlook.com>
2025-04-14 11:43:17 -0400

    local database config settings

--- src/db/DatabaseManager.java
+++ src/db/DatabaseManager.java
@@ -6,8 +6,11 @@ import java.sql.SQLException;
 
 public class DatabaseManager {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/reservation_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "200408"; // ajoute ton mot de passe si tu en as un
+    // private static final String USER = "root";
+    // private static final String PASSWORD = "200408"; // ajoute ton mot de passe si tu en as un
+
+    private static final String USER = "kevin";
+    private static final String PASSWORD = "reservation_billet_password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
